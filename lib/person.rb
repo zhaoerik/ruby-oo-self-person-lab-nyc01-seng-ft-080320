ec8b1e80731c3ca22796a315c6ec56f54ab9952e
Erik Zhao <erikzhao98@gmail.com>
2021-03-11 17:33:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,60 +1,62 @@
 require 'pry'
 class Person
     attr_reader :name 
-    attr_accessor :happiness_points, :hygiene_points, :bank_accounts
-    def initialize (name, bank_accounts=25, happiness_points = 8, hygiene_points =8)
+    attr_accessor :happiness, :hygiene, :bank_account
+    def initialize (name, bank_account=25, happiness = 8, hygiene =8)
         @name = name 
-        @bank_accounts = bank_accounts
-        @happiness_points = happiness_points
-        @hygiene_points = hygiene_points
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
     end
 
     def clean?
-        @point = hygiene_points
-        if @point > 7
-            "true"
+        if self.hygiene > 7
+            true
         else 
-            "false"
+            false
         end
     end
 
     def happy?
-        @happy_point = happiness_points
+        @happy_point = happiness
         if @happy_point > 7
-            "true"
+            true
         else
-            "false"
+            false
         end
     end
 
     def get_paid(salary)
-        @bank_accounts += salary
+        @bank_account += salary
         "all about the benjamins"
     end
 
     def take_bath
-        @hygiene_points += 4
+        @hygiene += 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        @happiness_points += 2
-        @hygiene_points -= 3
+        @happiness += 2
+        @hygiene -= 3
         "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend_name)
         @friend = friend_name
-        @happiness_points += 3
-        "Hi #{@friend}! It's #{@name}. How are you?"
+        @happiness += 3
+        @friend.happiness += 3
+        "Hi #{@friend.name}! It's #{@name}. How are you?"
     end
 
     def start_conversation(person ,topic)
         if topic == "politics"
-            @happiness_points -= 3
+            person.happiness -= 2
+            @happiness -= 2
             "blah blah partisan blah lobbyist"
         elsif topic == "weather"
-            @happiness_points += 3
+            @happiness += 1
+            person.happiness += 1
             "blah blah sun blah rain"
         else
             "blah blah blah blah blah"
