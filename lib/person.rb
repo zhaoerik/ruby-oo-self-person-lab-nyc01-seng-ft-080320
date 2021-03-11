1484b0fdd0d0a7bb3fdc672246f3fca11ca2e8af
Erik Zhao <erikzhao98@gmail.com>
2021-03-11 17:39:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,91 +1,69 @@
 class Person
 
-    attr_reader :name 
-    attr_accessor :happiness, :hygiene, :bank_account
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
 
-    def initialize(name)
-        @name = name
-        @bank_account = 25
-        @happiness = 8
-        @hygiene = 8
-    end
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
 
-    def bank_account(balance)
-        @bank_account = balance
-        if @bank_account < 0
-            @bank_account = 0
-        end
-    end
+  def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
 
-    def happiness(num)
-        @happiness = num
-        if @happiness > 10
-            @happiness = 10
-        elsif @happiness < 0
-            @happiness = 0
-        end     
-    end
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
 
-    def hygiene(num)
-        @hygiene = num
-        if @hygiene > 10
-            @hygiene = 10
-        elsif @hygiene < 0
-            @hygiene = 0
-        end
-    end
+  def happy?
+    happiness > 7
+  end
 
-    def clean?
-        if self.hygiene > 7
-            true
-        else 
-            false
-        end
-    end
+  def clean?
+    hygiene > 7
+  end
 
-    def happy?
-        @happy_point = happiness
-        if @happy_point > 7
-            true
-        else
-            false
-        end
-    end
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
 
-    def get_paid(salary)
-        @bank_account += salary
-        "all about the benjamins"
-    end
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
 
-    def take_bath
-        @hygiene += 4
-        "♪ Rub-a-dub just relaxing in the tub ♫"
+  def start_conversation(person, topic)
+    objects = [self, person]
+    if topic == "politics"
+      objects.each { |o| o.happiness -= 2}
+      first, second = ["partisan", "lobbyist"]
+    elsif topic == "weather"
+      objects.each { |o| o.happiness += 1}
+      first, second = ["sun", "rain"]
     end
+    first ||= "blah"
+    second ||= "blah"
+    base_string = "blah blah #{first} blah #{second}"
+  end
 
-    def work_out
-        @happiness += 2
-        @hygiene -= 3
-        "♪ another one bites the dust ♫"
-    end
+  def get_paid(salary)
+    self.bank_account += salary
+    self.happiness += 1
+    "all about the benjamins"
+  end
 
-    def call_friend(friend_name)
-        @friend = friend_name
-        @happiness += 3
-        @friend.happiness += 3
-        "Hi #{@friend.name}! It's #{@name}. How are you?"
-    end
+  def call_friend(friend)
+    [friend, self].each {|o| o.happiness += 3 }
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
 
-    def start_conversation(person ,topic)
-        if topic == "politics"
-            person.happiness -= 2
-            @happiness -= 2
-            "blah blah partisan blah lobbyist"
-        elsif topic == "weather"
-            @happiness += 1
-            person.happiness += 1
-            "blah blah sun blah rain"
-        else
-            "blah blah blah blah blah"
-        end
-    end
 end
